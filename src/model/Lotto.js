e121e1b747fbb2479276d8ca15b7529b6a0edd77
GGGGGangSub <dksrkdwns1411@daum.net>
2025-11-02 22:33:30 +0900

    feat(Lotto.js): Lotto 클래스 생성 및 유효성 검사 기능 구현

---
+++ src/model/Lotto.js
@@ -0,0 +1,27 @@
+class Lotto {
+  #numbers;
+
+  constructor(numbers) {
+    this.#validate(numbers);
+    this.#numbers = numbers;
+  }
+
+  #validate(numbers) {
+    if (numbers.length !== 6) {
+      throw new Error(`[ERROR] 로또 번호는 6개여야 합니다.`);
+    }
+    if (numbers.some((number) => !Number.isInteger(number))) {
+      throw new Error(`[ERROR] 로또 번호는 정수여야 합니다.`);
+    }
+    if (new Set(numbers).size !== 6) {
+      throw new Error('[ERROR] 로또 번호에 중복된 숫자가 있습니다.');
+    }
+    if (numbers.some((n) => n < 1 || n > 45)) {
+      throw new Error(`[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.`);
+    }
+  }
+
+  // TODO: 추가 기능 구현
+}
+
+export default Lotto;
